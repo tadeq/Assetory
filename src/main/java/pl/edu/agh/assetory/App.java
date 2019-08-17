50febc225fe16eafcbeeef1fcd1d3ca52112d590
Marek Gargas <250497@wp.pl>
2019-08-17 20:30:16 +0200

    fix query logic

--- src/main/java/pl/edu/agh/assetory/App.java
+++ src/main/java/pl/edu/agh/assetory/App.java
@@ -36,43 +36,81 @@ public class App implements CommandLineRunner {
     }
 
     private void prepareTestStructure() {
+
         Category categoryAll = categoriesService.addCategory(Category.builder()
-                .name("all")
-                .addAttribute("user", AttributeType.text)
-                .addAttribute("location", AttributeType.text)
-                .addAttribute("price", AttributeType.number)
+                .name("All")
+                .addAttribute("User", AttributeType.text)
+                .addAttribute("Location", AttributeType.text)
+                .addAttribute("Price", AttributeType.number)
                 .build());
         Category hardware = categoriesService.addCategory(Category.builder()
                 .parentCategoryId(categoryAll.getId())
-                .name("hardware")
-                .addAttribute("manufacturer", AttributeType.text)
+                .name("Hardware")
+                .addAttribute("Manufacturer", AttributeType.text)
                 .build());
         Category software = categoriesService.addCategory(Category.builder()
                 .parentCategoryId(categoryAll.getId())
-                .name("software")
-                .addAttribute("expirationDate", AttributeType.date)
+                .name("Software")
+                .addAttribute("ExpirationDate", AttributeType.date)
+                .build());
+        Category subsoftware1 = categoriesService.addCategory(Category.builder()
+                .parentCategoryId(software.getId())
+                .name("Sub-Software-1")
+                .build());
+        Category subsoftware2 = categoriesService.addCategory(Category.builder()
+                .parentCategoryId(software.getId())
+                .name("Sub-Software-2")
                 .build());
         categoriesService.updateCategory(Category.builder()
                 .from(categoryAll)
                 .addSubcategoryIds(Lists.newArrayList(hardware.getId(), software.getId()))
                 .build());
+        categoriesService.updateCategory(Category.builder()
+                .from(software)
+                .addSubcategoryIds(Lists.newArrayList(subsoftware1.getId(), subsoftware2.getId()))
+                .build());
         Asset computer = assetsService.addAsset(Asset.builder()
                 .categoryId(hardware.getId())
                 .name("Computer")
-                .addAttribute(AttributeType.text, "user", "John")
-                .addAttribute(AttributeType.text, "location", "office")
-                .addAttribute(AttributeType.number, "price", "2500")
-                .addAttribute(AttributeType.text, "manufacturer", "Lenovo")
+                .addAttribute(AttributeType.text, "User", "John")
+                .addAttribute(AttributeType.text, "Location", "Office")
+                .addAttribute(AttributeType.number, "Price", "2500")
+                .addAttribute(AttributeType.text, "Manufacturer", "lenovo")
+                .build());
+        Asset laptop = assetsService.addAsset(Asset.builder()
+                .categoryId(hardware.getId())
+                .name("Laptop")
+                .addAttribute(AttributeType.text, "User", "Johnatan")
+                .addAttribute(AttributeType.text, "Location", "Office2")
+                .addAttribute(AttributeType.number, "Price", "100")
+                .addAttribute(AttributeType.text, "Manufacturer", "lenovo")
                 .build());
         Asset windows = assetsService.addAsset(Asset.builder()
                 .categoryId(software.getId())
                 .name("Windows")
-                .addAttribute(AttributeType.text, "location", "computer1")
-                .addAttribute(AttributeType.text, "user", "John2")
-                .addAttribute(AttributeType.number, "price", "100")
-                .addAttribute(AttributeType.date, "expirationDate", "7-07-2020")
+                .addAttribute(AttributeType.text, "Location", "Computer1")
+                .addAttribute(AttributeType.text, "User", "John2")
+                .addAttribute(AttributeType.number, "Price", "100")
+                .addAttribute(AttributeType.date, "ExpirationDate", "7-07-2020")
+                .build());
+        Asset subwindows1 = assetsService.addAsset(Asset.builder()
+                .categoryId(subsoftware1.getId())
+                .name("Windows")
+                .addAttribute(AttributeType.text, "Location", "Computer2")
+                .addAttribute(AttributeType.text, "User", "John")
+                .addAttribute(AttributeType.number, "Price", "2500")
+                .addAttribute(AttributeType.date, "ExpirationDate", "14-07-2020")
+                .build());
+        Asset subwindows2 = assetsService.addAsset(Asset.builder()
+                .categoryId(subsoftware2.getId())
+                .name("Windows")
+                .addAttribute(AttributeType.text, "Location", "Computer1")
+                .addAttribute(AttributeType.text, "User", "Johnatan")
+                .addAttribute(AttributeType.number, "Price", "1700")
+                .addAttribute(AttributeType.date, "ExpirationDate", "14-07-2020")
                 .build());
         System.out.println(" ");
+
     }
 
 }
