2e1b44374bdaa29758fcb55ecdb681e77ef5b685
Marek Gargas <250497@wp.pl>
2019-10-28 18:51:12 +0100

    add possibility to run app locally

--- src/main/java/pl/edu/agh/assetory/utils/Config.java
+++ src/main/java/pl/edu/agh/assetory/utils/Config.java
@@ -1,13 +1,11 @@
 package pl.edu.agh.assetory.utils;
 
 import org.apache.http.HttpHost;
-import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,21 +23,21 @@ public class Config {
     private String password;
 
 
-
     @Bean(destroyMethod = "close")
     RestHighLevelClient client() {
 
-        final CredentialsProvider credentialsProvider =
-                new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(username, password));
-
-
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost(elasticsearchHost)).setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
-                        .setDefaultCredentialsProvider(credentialsProvider)));
-
-        return client;
-
+        if (elasticsearchHost.equals("")) {
+            HttpHost localhost = new HttpHost("localhost", 9200);
+            return new RestHighLevelClient(RestClient.builder(localhost));
+        } else {
+            final CredentialsProvider credentialsProvider =
+                    new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY,
+                    new UsernamePasswordCredentials(username, password));
+            return new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(elasticsearchHost))
+                            .setHttpClientConfigCallback(httpClientBuilder ->
+                                    httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)));
+        }
     }
 }
